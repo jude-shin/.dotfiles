4ba13740a44d59a6a0b0b60b5452d43306b040fa
JimmyChin946 <Judes946@gmail.com>
2025-03-28 00:28:16 -0700

    prompts

--- update.sh
+++ update.sh
@@ -1,86 +1,70 @@
-#!bin/bash
+#!/bin/bash
 
 # $XDG_CONFIG_HOME
 # $HOME
-NEW_PATH="./dotfiles"
+NEW_PATH="./dotfiles/"
 CSV_PATH="./paths.csv"
 
-# if there are command line arguments, then go through each of those in a for loop
-if [$# -eq 0]; then 
-	for name_arg in "$@"
-	do
-		# find the line in the csv with that name
-	tail -n +2 "$CSV_PATH" | while IFS=',' read -r name new path 
-		do
-    	if [[ "$name" == "$name_arg" ]]; then
-				# you have found the line, then you can process this download
-				install_dotfile $name $new $path
-				break
-    	fi
-		done 
-	done
-# else, there are no command line arguments, go through all of the items in the csv, and run the script
-else 
-# Skip the header line
-	tail -n +2 "$CSV_PATH" | while IFS=',' read -r name new path 
-	do
-		install_dotfile $name $new $path
-	done 
-fi
+function INSTALL_DOTFILE {
+    # args: name, new, path
+    local name=$1
+    local new=$2
+    local path=$3
 
+    CONFIRM_INSTALL "$name" "$new" "$path"
+    local confirm_status=$?
 
-
-
-
-install_dotfile() {
-	# args: name, new, path
-	local name=$1
-	local new=$2
-	local path=$3
-	confirm_install $name $new $path
-
-	if [$? = 0]; then
-		# the user canceled this install, and you should skip it
-		echo "SKIPPING $name dotfile..."
-	elif [$? = 1]; then
-		# the user wants to install this, so go ahead and install it
-		echo "INSTALLING new $name dotfile..."
-	fi
+    if [ $confirm_status -eq 0 ]; then
+        # the user canceled this install, and you should skip it
+        echo "SKIPPING $name dotfile..."
+    elif [ $confirm_status -eq 1 ]; then
+        # the user wants to install this, so go ahead and install it
+        echo "INSTALLING new $name dotfile..."
+    fi
 }
 
-confirm_install() {
-	# args: name, new, path
-	local name=$1
-	local new=$2
-	local path=$3
-	
-	echo "Do you want to install the $name dotfile?" 
-	read -p "WARNING, may overwrite files! (y/n): " response 
+function CONFIRM_INSTALL {
+    # args: name, new, path
+    local name=$1
+    local new=$2
+    local path=$3
 
-	if [$response -eq "y"]; then
-		return 1
-	elif [$response -eq "n"]; then
-		return 0
-	else
-		echo "Please enter 'y' or 'n' as a response"
-		confirm_install $name $new $path
-		return $?
-	fi
+    echo "Do you want to install the $name dotfile?"
+
+    while true; do
+        # Explicitly prompt for user input
+        read -p "WARNING, may overwrite files! (y/n): " response
+
+        if [[ "$response" == "y" ]]; then
+            return 1  # Proceed with install
+        elif [[ "$response" == "n" ]]; then
+            return 0  # Skip install
+        else
+            echo "Please enter 'y' or 'n' as a response"
+        fi
+    done
 }
 
+# Read the CSV into an array using mapfile
+mapfile -t lines < "$CSV_PATH"
 
+# Process each line after the header (skip the first line)
+for ((i = 1; i < ${#lines[@]}; i++)); do
+    # Read each line and split by comma
+    line="${lines[$i]}"
+    name=$(echo "$line" | cut -d',' -f1 | xargs)
+    new=$(echo "$line" | cut -d',' -f2 | xargs)
+    path=$(echo "$line" | cut -d',' -f3 | xargs)
 
+    # Skip empty lines
+    if [[ -z "$name" || -z "$new" || -z "$path" ]]; then
+        continue
+    fi
 
+    # Debug: Print the line read from CSV
+    echo "Reading: $name, $new, $path"  # Debug: Show CSV line
 
-
-
-
-
-
-
-
-
-
-
-
+    # Now process the remaining lines
+    INSTALL_DOTFILE "$name" "$new" "$path"
+done
 
